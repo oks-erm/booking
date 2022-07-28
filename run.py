1b1e514c3e80357a0e44650585c0411b9dc8e4a4
oks-erm <oks.erm.012@gmail.com>
2022-07-28 22:27:30 +0000

    Add staff_login function to sign in/up a member of staff

--- run.py
+++ run.py
@@ -17,7 +17,8 @@ def create_staff():
     """
     Creates a new member of Staff
     """
-    print("New")
+    new_name = input("Enter a name for a new member of staff:\n")
+    print(f"Hi, {new_name}!")
 
 
 def get_staff():
@@ -27,7 +28,7 @@ def get_staff():
     try:
         staff_data = SHEET.worksheet('staff').get_all_values()
         staff = [x[0] for x in staff_data[1:]]
-        print(staff)
+        return staff
     except gspread.exceptions.WorksheetNotFound:
         print("Sorry, something went wrong accessing database")
         yes_or_no = input("press 1 - Try again?\npress 2 - Enter as new\n")
@@ -37,4 +38,31 @@ def get_staff():
             create_staff()      
 
 
-get_staff()
+def validate_user(name):
+    return True
+
+
+def staff_login():
+    """
+    Logs in a member of staff
+    """
+    staff = get_staff()
+    print("\n\n\t\tWelcome to Your Booking System!\n")
+    while True:
+        entered_name = input(
+            "Enter your name or enter 'new' if you are a new member of staff: "
+        )
+        if entered_name in staff:
+            if validate_user(entered_name):
+                print(f"Hi, {entered_name}!")
+                break
+        elif entered_name.lower() == "new":
+            create_staff()
+            break
+        else:
+            print(f"Sorry, there is no user '{entered_name}'.")
+            print("If you want to create a new user, enter 'new'\n")
+
+
+staff_login()
+print("Move on")
