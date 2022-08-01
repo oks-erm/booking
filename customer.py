614ea49ad097c220bf082fe26da13602671b353e
oks-erm <oks.erm.012@gmail.com>
2022-08-01 14:21:14 +0000

    Add create_customer fucntion that creates a customer with a given name

--- customer.py
+++ customer.py
@@ -1,10 +1,11 @@
 """
 Includes customers specific functions.
 """
-from spreadsheet import get_data
+from spreadsheet import get_data, update_worksheet
 
 
-customers = get_data("customers")
+customers = get_data("customers")[1:]
+KEYS = get_data("customers")[0]
 
 
 def search(name, data):
@@ -17,6 +18,20 @@ def search(name, data):
             return element
 
 
+def create_customer(name):
+    """
+    Creates a new customer with a parameter 'name' and writes
+    it to the spreadsheet.
+    """
+    new_name = name
+    phone = input("Enter contact number: ")
+    email = input("Enter email to receive reminders: ")
+    bday = input("Enter date of birth: ")
+    new_data = [new_name, phone, email, bday, 1, ""]
+    update_worksheet(new_data, "customers")
+    return dict(zip(KEYS, new_data))
+
+
 def get_customer():
     """
     Checks if the customer exists and returns
@@ -26,4 +41,4 @@ def get_customer():
     names = [dct['NAME'] for dct in customers]
     if user_inp in names:
         return search(user_inp, customers)
-    return create_customer()
+    return create_customer(user_inp)
