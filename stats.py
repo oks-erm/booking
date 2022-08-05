e7a8567282cb7cea0f37aa08744a6d8612cc28cd
oks-erm <oks.erm.012@gmail.com>
2022-08-05 00:15:44 +0000

    Add upload fucntion to upload pdf with stats to GDrive

--- stats.py
+++ stats.py
@@ -3,7 +3,30 @@ from datetime import date, datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from spreadsheet import get_worksheet
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from spreadsheet import get_worksheet, SCOPED_CRED
+
+
+def upload():
+    try:
+        # Authenticate and construct service.
+        service = build('drive', 'v3', credentials=SCOPED_CRED)
+
+        file_metadata = {'st': 'stats.pdf', 
+                         'parents': ['1RMQBmiL3ATEkIAtPFmQypM5rcYfzXsD-']}
+        media = MediaFileUpload('stats.pdf',
+                                mimetype='application/pdf')
+        file = service.files().create(body=file_metadata, media_body=media,
+                                      fields='id').execute()
+        print(F'File ID: {file.get("id")}')
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        file = None
+
+    return file.get('id')
 
 
 def age(birthdate):
@@ -34,22 +57,12 @@ def customers_stats():
     Writes customers stats in pdf file.
     """
     df = pd.read_csv('stats.csv')
+    # histogram with age
     plt.figure()
     plt.hist(df["BD"], bins=20, color="blue", edgecolor="black")
     plt.gca().yaxis.set_major_formatter(plt.FuncFormatter('{}%'.format))
     plt.xlabel('Age')
-
-    plt.figure()
-    plt.pie([(df['NUM OF BOOKINGS'].sum()-df['CANCELLED'].sum()),
-            df['CANCELLED'].sum()], autopct='%1.1f%%',
-            labels=["Total number of bookings", "Cancelled"])
-    plt.xlabel('Bookings vs cancelled')
-
-    plt.figure()
-    plt.hist(df['NUM OF BOOKINGS'], bins=20, color="green", edgecolor="black")
-    plt.gca().yaxis.set_major_formatter(plt.FuncFormatter('{}%'.format))
-    plt.xlabel('Number of bookings per customer')
-
+    # pie chart with age groups
     plt.figure()
     x = pd.DataFrame(df)
     bins = [0, 20, 30, 40, 50, 60, 120]
@@ -58,7 +71,18 @@ def customers_stats():
     pie_data = (x.groupby("Age groups")["BD"].count()).to_frame()
     pie_data["Age groups"] = round((pie_data.BD/sum(pie_data.BD))*100, 2)
     pie_data["Age groups"].plot.pie(autopct='%1.1f%%')
-
+    # rate of cancelled bookings 
+    plt.figure()
+    plt.pie([(df['NUM OF BOOKINGS'].sum()-df['CANCELLED'].sum()),
+            df['CANCELLED'].sum()], autopct='%1.1f%%',
+            labels=["Total number of bookings", "Cancelled"])
+    plt.xlabel('Bookings vs cancelled')
+    # histogram number of bookings per customer
+    plt.figure()
+    plt.hist(df['NUM OF BOOKINGS'], bins=20, color="green", edgecolor="black")
+    plt.gca().yaxis.set_major_formatter(plt.FuncFormatter('{}%'.format))
+    plt.xlabel('Number of bookings per customer')
+    # save multiple figures to one pdf file
     pdf_pgs = PdfPages("stats.pdf")
     fig_nums = plt.get_fignums()
     figs = [plt.figure(n) for n in fig_nums]
