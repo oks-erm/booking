f55550d980f320c7989a8e5c3e01e545164ede3f
oks-erm <oks.erm.012@gmail.com>
2022-08-19 13:38:34 +0000

    Add tests for stats module

--- booking_sys/tests/test_stats.py
+++ booking_sys/tests/test_stats.py
@@ -4,7 +4,8 @@
 import pytest
 import os
 from unittest import mock
-import booking_sys.stats as stats 
+from booking_sys.stats import data_for_stats, calculate_age
+from run import cleanup
 
 
 @pytest.mark.parametrize("a, expected",
@@ -22,12 +23,24 @@ import booking_sys.stats as stats
                           ("10-18-2023", ("Make sure you use "
                                           "dd-mm-yyyy format"))])
 def test_calculate_age(a, expected):
-    assert stats.calculate_age(a) == expected
+    """
+    Test calculate_age on different values and data types.
+    """
+    assert calculate_age(a) == expected
 
 
-# @mock.patch("booking_sys.stats.get_worksheet",
-#             return_value=[['NAME', 'PHONE', 'EMAIL', 'BIRTHDATE'],
-#                           ['Bob', '003543243422', "q@w.er", "10-10-1976"],
-#                           ['Kelly', '+44 6734657788', "t@y.ui", "10-11-1987"]])
-# def test_data_for_stats(*args): 
-#     assert os.path.exists("stats.csv") is True
+@mock.patch("booking_sys.stats.get_worksheet",
+            return_value=[['NAME', 'PHONE', 'EMAIL', 'BIRTHDATE'],
+                          ['Bob', '003543243422', "q@w.er", "10-10-1976"],
+                          ['Kelly', '+44 6734657788', "t@y.ui", "10-11-1987"]])
+def test_data_for_stats(*args): 
+    """
+    Test if a file with required data is created.
+    """
+    data_for_stats()
+    if os.path.exists("stats.csv"):
+        with open("stats.csv", 'r') as file:
+            content = file.readlines()
+            assert 'NAME,PHONE,EMAIL,BIRTHDATE\n' in content
+    assert os.path.exists("stats.csv") is True
+    cleanup()
