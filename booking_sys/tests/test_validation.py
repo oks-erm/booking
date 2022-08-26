50da351d9b0b7d875223302e0793efa030f95f5d
oks-erm <oks.erm.012@gmail.com>
2022-08-26 23:27:18 +0000

    Update formatting in test_validation

--- booking_sys/tests/test_validation.py
+++ booking_sys/tests/test_validation.py
@@ -7,27 +7,36 @@ from booking_sys.validation import (to_date, convert_date, date_input,
                                     birthdate, time_input, email, phone_num)
 
 
-def test_to_date_date_formats():
+@pytest.mark.parametrize("a, expected",
+                         [("10-10-2022", date(2022, 10, 10)),
+                          ("1-10-2022", date(2022, 10, 1)),
+                          ("01-10-2022", date(2022, 10, 1)),
+                          ("10/10/2022", False),
+                          ("10.10.2022", False),
+                          ("30-02-2022", False),
+                          ("10-20-2022", False),
+                          ("10-10-22", False)])
+def test_to_date_date_formats(a, expected):
     """
     Test date formats.
     """
-    assert to_date("10-10-2022") == date(2022, 10, 10)
-    assert to_date("1-10-2022") == date(2022, 10, 1)
-    assert to_date("01-10-2022") == date(2022, 10, 1)
-    formats = ["10/10/2022", "10.10.2022", "30-02-2022",
-               "10-20-2022", "10-10-22"]
-    for item in formats:
-        assert to_date(item) is False
+    assert to_date(a) == expected
 
 
-def test_to_date_data_formats():
+@pytest.mark.parametrize("a, expected",
+                         [("string", False),
+                          (0, False),
+                          (["10-10-2022"], False),
+                          (("10-10-2022",), False),
+                          ({"date": "10-10-2022"}, False),
+                          (None, False),
+                          (True, False),
+                          (False, False)])
+def test_to_date_data_formats(a, expected):
     """
     Test data formats.
     """
-    fail = ["string", 0, ["10-10-2022"], ("10-10-2022",),
-            {"date": "10-10-2022"}, None, True, False]
-    for item in fail:
-        assert to_date(item) is False
+    assert to_date(a) == expected
 
 
 @pytest.mark.parametrize("a, expected",
@@ -44,135 +53,198 @@ def test_convert_date(a, expected):
     assert convert_date(a) == expected
 
 
-def test_convert_date_data_formats():
+@pytest.mark.parametrize("a, expected",
+                         [(42, False),
+                          (0, False),
+                          (["10/10/2022"], False),
+                          (("10/10/2022",), False),
+                          ({"date": "10/10/2022"}, False),
+                          (None, False),
+                          (True, False),
+                          (False, False)])
+def test_convert_date_data_formats(a, expected):
     """
     Test different data formats.
     """
-    fail = [42, 0, ["10/10/2022"], ("10/10/2022",),
-            {"date": "10/10/2022"}, None, True, False]
-    for item in fail:
-        assert convert_date(item) is False
+    assert convert_date(a) == expected
 
 
-def test_date_input():
+@pytest.mark.parametrize("a, expected",
+                         [("10/10/2023", "10-10-2023"),
+                          ("10.10.2023", "10-10-2023"),
+                          ("10-10-2023", "10-10-2023"),
+                          ("10,10,2023", "10-10-2023"),
+                          ("10.10.2021", False),
+                          ("1.10.2022", False)])
+def test_date_input(a, expected):
     """
     Test different date formats.
     """
-    assert date_input("10/10/2023") == "10-10-2023"
-    assert date_input("10.10.2023") == "10-10-2023"
-    assert date_input("10-10-2023") == "10-10-2023"
-    assert date_input("10,10,2023") == "10-10-2023"
-    assert date_input("10.10.2021") is False
-    assert date_input("1.10.2022") is False
+    assert date_input(a) == expected
 
 
-def test_date_input_data_formats():
+@pytest.mark.parametrize("a, expected",
+                         [(42, False),
+                          (0, False),
+                          (["10/10/2022"], False),
+                          (("10/10/2022",), False),
+                          ({"date": "10/10/2022"}, False),
+                          (None, False),
+                          (True, False),
+                          (False, False)])
+def test_date_input_data_formats(a, expected):
     """
     Test different data formats.
     """
-    fail = [42, 0, ["10/10/2022"], ("10/10/2023",),
-            {"date": "10/10/2022"}, None, True, False]
-    for item in fail:
-        assert date_input(item) is False
+    assert date_input(a) == expected
 
 
-def test_birthdate():
+@pytest.mark.parametrize("a, expected",
+                         [("10/10/2000", "10-10-2000"),
+                          ("10.10.2000", "10-10-2000"),
+                          ("10-10-2000", "10-10-2000"),
+                          ("10,10,2000", "10-10-2000"),
+                          ("10.10.2023", False),
+                          ("1.10.2022", False)])
+def test_birthdate(a, expected):
     """
     Test different date formats.
     """
-    assert birthdate("10/10/2000") == "10-10-2000"
-    assert birthdate("10.10.2000") == "10-10-2000"
-    assert birthdate("10-10-2000") == "10-10-2000"
-    assert birthdate("10,10,2000") == "10-10-2000"
-    assert birthdate("10.10.2023") is False
-    assert birthdate("1.10.2021") is False
+    assert birthdate(a) == expected
 
 
-def test_birthdate_data_formats():
+@pytest.mark.parametrize("a, expected",
+                         [(42, False),
+                          (0, False),
+                          (["10/10/2022"], False),
+                          (("10/10/2022",), False),
+                          ({"date": "10/10/2022"}, False),
+                          (None, False),
+                          (True, False),
+                          (False, False)])
+def test_birthdate_data_formats(a, expected):
     """
     Test different data formats.
     """
-    fail = [42, 0, ["10/10/2000"], ("10/10/2000",),
-            {"date": "10/10/2000"}, None, True, False]
-    for item in fail:
-        assert birthdate(item) is False
+    assert birthdate(a) == expected
 
 
-def test_time_inpit():
+@pytest.mark.parametrize("a, expected",
+                         [("18:00", True),
+                          ("25:00", False),
+                          ("1:00", False),
+                          ("111:00", False)])
+def test_time_inpit(a, expected):
     """
     Test different time formats.
     """
-    assert time_input("18:00") is True
-    assert time_input("25:00") is False
-    assert time_input("1:00") is False
-    assert time_input("111:00") is False
+    assert time_input(a) == expected
 
 
-def test_time_input_data_formats():
+@pytest.mark.parametrize("a, expected",
+                         [(42, False),
+                          (0, False),
+                          (["18:00"], False),
+                          (("18:00",), False),
+                          ({"time": "18:00"}, False),
+                          (None, False),
+                          (True, False),
+                          (False, False)])
+def test_time_input_data_formats(a, expected):
     """
     Test different data formats.
     """
-    fail = [42, 0, ["18:00"], ("18:00",), {"time": "18:00"},
-            None, True, False]
-    for item in fail:
-        assert time_input(item) is False
+    assert time_input(a) == expected
 
 
-def test_email():
+@pytest.mark.parametrize("a, expected",
+                         [("email@a.bc", True),
+                          ("string", False),
+                          ("@string", False),
+                          ("string@", False),
+                          ("str@ing", False),
+                          ("str@ing.", False),
+                          ("stri.ng", False),
+                          (".str@i.ng", False),
+                          ("@str@i.ng", False),
+                          ("@str@in.g", False)])
+def test_email(a, expected):
     """
     Test different email formats.
     """
-    assert email("email@a.bc") is True
-    emails = ["string", "@string", "string@",
-              "str@ing", "str@ing.", "stri.ng",
-              ".str@i.ng", "@str@i.ng", "@str@in.g"]
-    for item in emails:
-        assert email(item) is False
+    assert email(a) == expected
 
 
-def test_email_data_formats():
+@pytest.mark.parametrize("a, expected",
+                         [(42, False),
+                          (0, False),
+                          (["email@a.bc"], False),
+                          (("email@a.bc",), False),
+                          ({"email": "email@a.bc"}, False),
+                          (None, False),
+                          (True, False),
+                          (False, False)])
+def test_email_data_formats(a, expected):
     """
     Test different data formats.
     """
-    fail = [42, 0, ["email@a.bc"], ("email@a.bc",),
-            {"email": "email@a.bc"}, None, True, False]
-    for item in fail:
-        assert email(item) is False
+    assert email(a) == expected
 
 
-def test_phone_num():
+valid = ["+1 234567", "+22 345678", "+323 456789",
+         "+42345 67890", "+52345 678901", "+6123456 78901",
+         "+71234567 89012", "+812345678 90123", "+91234567890.1",
+         "+1 23 45 67", "+22 345 678", "+323 4567 89",
+         "+42 345 678 90", "+523 45 67 89 01", "+612 3 45 6 789 01",
+         "+71-234-567-8", "+812-345-678.123", "+9(235)67-90.1",
+         "003 456 78", "0023 456789", "002 34 56 78 9",
+         "002345 67890", "002345 678901", "00123456 78901",
+         "001234567 89012", "0012345678 90123", "001234567890.1",
+         "0023 45 67", "002 345 678", "0023 4567 89",
+         "002 345 678 90", "0023 45 67 89 01", "0012 3 45 6 789 01",
+         "001-234-567-8", "0012-345-678.123", "+523.4567.89 01"]
+pairs_valid = []
+for item in valid:
+    pairs_valid.append((item, True))
+
+invalid = ["1234567", "022 345678", "+023 456789", "423 45 67 890", 
+           "+002345 678901", "+6123456+78901", "+(712)34567 89012",
+           "+81234567888901238786", "-912345678901", "-1 23 45 67.1",
+           "22.345.678", "323-4567-89", "+42 345", "+612,3 45 6,789 01",
+           "00 234567", "stringstring", "00(235)67-90.1", ""]
+pairs_invalid = []
+for item in invalid:
+    pairs_invalid.append((item, False))
+
+
+@pytest.mark.parametrize("a, expected",
+                         pairs_valid)
+def test_phone_num_valid(a, expected):
     """
-    Test different phone formats.
+    Test valid phone formats.
     """
-    valid = ["+1 234567", "+22 345678", "+323 456789",
-             "+42345 67890", "+52345 678901", "+6123456 78901",
-             "+71234567 89012", "+812345678 90123", "+91234567890.1",
-             "+1 23 45 67", "+22 345 678", "+323 4567 89",
-             "+42 345 678 90", "+523 45 67 89 01", "+612 3 45 6 789 01",
-             "+71-234-567-8", "+812-345-678.123", "+9(235)67-90.1",
-             "003 456 78", "0023 456789", "002 34 56 78 9",
-             "002345 67890", "002345 678901", "00123456 78901",
-             "001234567 89012", "0012345678 90123", "001234567890.1",
-             "0023 45 67", "002 345 678", "0023 4567 89",
-             "002 345 678 90", "0023 45 67 89 01", "0012 3 45 6 789 01",
-             "001-234-567-8", "0012-345-678.123", "+523.4567.89 01"]
-    for phone in valid:
-        assert phone_num(phone) is True
-
-    invalid = ["1234567", "022 345678", "+023 456789",
-               "423 45 67 890", "+002345 678901", "+6123456+78901",
-               "+(712)34567 89012", "+81234567888901238786", "-912345678901",
-               "-1 23 45 67.1", "22.345.678", "323-4567-89",
-               "+42 345", "+612,3 45 6,789 01",
-               "00 234567", "stringstring", "00(235)67-90.1", ""]
-    for phone in invalid:
-        assert phone_num(phone) is False
+    assert phone_num(a) == expected
 
 
-def test_phone_num_data_formats():
+@pytest.mark.parametrize("a, expected",
+                         pairs_invalid)
+def test_phone_num_invalid(a, expected):
+    """
+    Test invalid phone formats.
+    """
+    assert phone_num(a) == expected
+
+
+@pytest.mark.parametrize("a, expected",
+                         [(42, False),
+                          ([], False),
+                          ((), False),
+                          ({}, False),
+                          (None, False),
+                          (True, False),
+                          (False, False)])
+def test_phone_num_data_formats(a, expected):
     """
     Test different data formats.
     """
-    data = [None, True, False, 42, [], {}, ()]
-    for item in data:
-        assert phone_num(item) is False
+    assert phone_num(a) == expected
