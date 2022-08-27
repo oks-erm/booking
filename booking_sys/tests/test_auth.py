7dcc1c92b541c027324c330abe73e0f8468e583d
oks-erm <oks.erm.012@gmail.com>
2022-08-27 13:46:57 +0000

    Update formatting in test_auth

--- booking_sys/tests/test_auth.py
+++ booking_sys/tests/test_auth.py
@@ -7,7 +7,7 @@ from booking_sys.auth import authorise, check_password, staff_login
 
 def test_check_password():
     """
-    Test check_password on different values.
+    Tests check_password on different values.
     """
     user = {'NAME': 'Bob', 'PASSWORD': '123', 'CONTACT': ''}
     password = "123"
@@ -23,7 +23,7 @@ def test_check_password():
 @mock.patch("getpass.getpass")
 def test_authorise_seq(getpass):
     """
-    Test authorisation on different sequences of values.
+    Tests authorisation on different sequences of values.
     """
     user = {'NAME': 'Bob', 'PASSWORD': '123', 'CONTACT': ''}
     getpass.side_effect = ["11", "123", "12"]
@@ -36,26 +36,37 @@ def test_authorise_seq(getpass):
     assert authorise(user) is False
 
 
-@mock.patch("booking_sys.staff.create_staff",
-            return_value={'NAME': 'B', 'PASSWORD': '1', 'CONTACT': ''})
-@mock.patch('builtins.input', return_value='new')
-def test_staff_login_for_new(input, create_staff):
+@mock.patch("booking_sys.staff.create_staff")
+@mock.patch('builtins.input')
+def test_staff_login_for_new(*args):
     """
     Tests a function for a new user.
     """
+    (mock_input, mock_create) = args
+    mock_input.return_value = 'new'
+    mock_create.return_value = {'NAME': 'B', 'PASSWORD': '1', 'CONTACT': ''}
     data = [{'NAME': 'Bob', 'PASSWORD': '123', 'CONTACT': ''}]
     result = staff_login(data)
+
     assert isinstance(result, dict) is True
-    assert result == create_staff.return_value
+    mock_input.assert_called()
+    mock_create.assert_called()
+    assert result == mock_create.return_value
 
 
-@mock.patch("booking_sys.auth.authorise", return_value=True)
-@mock.patch('builtins.input', return_value='Bob')
+@mock.patch("booking_sys.auth.authorise")
+@mock.patch('builtins.input')
 def test_staff_login_for_existing(*args):
     """
     Tests a function for an existing user.
     """
+    (mock_input, mock_auth) = args
+    mock_input.return_value = 'Bob'
+    mock_auth.return_value = True
     data = [{'NAME': 'Bob', 'PASSWORD': '123', 'CONTACT': ''}]
     result = staff_login(data)
+
     assert isinstance(result, dict) is True
+    mock_input.assert_called()
+    mock_auth.assert_called()
     assert result == data[0]
